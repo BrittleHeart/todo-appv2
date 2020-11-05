b4c8bdebdc7720db82dbae63f6b6fd38c7c2177b
Bartosz Pazdur <b.pazdur@onet.pl>
2020-11-05 13:04:09 +0100

    Created login route and installed joi

--- layout/src/views/Login.vue
+++ layout/src/views/Login.vue
@@ -1,18 +1,102 @@
 <template>
-  <div class="home">
-    <h1>This is an login page</h1>
+  <div class="login">
+    <p>{{formError}}</p>
+    <form @submit.prevent="login()">
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input 
+          type="text" 
+          name="email" 
+          id="email" 
+          placeholder="Type your email.." 
+          v-model="user.email" 
+          required 
+          role="input">
+        
+        <h5 class="text-muted">Type your email (min: 5 characters):</h5>
+      </div>
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input 
+          type="password" 
+          name="password" 
+          id="password" 
+          placeholder="Type your password.." 
+          v-model="user.password" 
+          required 
+          role="input">
+        <h5 class="text-muted">Type your email (min: 7 characters):</h5>
+
+      </div>
+      <button type="submit" class="button">Sign in</button>
+    </form>
   </div>
 </template>
 
 <script>
-import * as yup from 'yup'
+import Joi from 'joi'
+import axios from 'axios'
+
+const schema = Joi.object().keys({
+  email: Joi.string().trim().min(5).max(255).email({minDomainSegments: 2, tlds: {allow: ['pl', 'com']}}).required(),
+  password: Joi.string().trim().min(7).max(255).required()
+})
 
 export default {
   data: () => ({
     user: {
       email: '',
       password: '',
+    },
+    formError: '',
+    loggedIn: false
+  }),
+  methods: {
+    async login() {
+      this.formError = '' 
+
+      if(this.isUserValid()) {
+        this.loggedIn = true
+
+        try {
+          const response = await axios.post('http://localhost:3000/api/v1/authenticate', this.user)
+
+          if(!response.data.access_token)
+            return this.formError = "Invalid credentials"
+
+          switch(response.status)
+          {
+            case 400:
+              this.formError = "Bad request"
+              break;
+            case 500:
+              this.formError = "Server error"
+              break;
+          }
+
+          if(response.status === 200 && response.data.access_token) {
+            window.localStorage.setItem("token", response.data.access_token)
+            return this.$router.push('/dashboard')
+          }
+
+        } catch(error) {
+          return this.formError = error
+        }
+      }
+    },
+    isUserValid() {
+      const validateSchema = schema.validate(this.user)
+
+      if(!validateSchema.error)
+        return true;
+        
+      if(validateSchema.error.message.includes('email'))
+        this.formError = 'Email value must valid'
+      else
+        this.formError = 'Password value must valid'
+        
+      return false
     }
-  })
+  }
 }
 </script>
